2b5663161f5e32406fd16e97fa15cf5eab048776
Vinicius <vinicius.valle@altatech.com.br>
2024-07-04 15:51:12 -0300

    Componentes excluidos que não estão sendo usados.

--- src/app/fonts/font.ts
+++
@@ -1,7 +0,0 @@
-// import  localFont from 'next/font/local'
-
-// export const font_primary = localFont({
-//   src: './fonts/mona-sans/TTF/Mona-Sans-BoldWide.ttf',
-//   display: 'swap'
-// })
-// 
--- src/components/form/button.tsx
+++
@@ -1,9 +0,0 @@
-import { font_primary } from '@/app/page'
-
-export default function Button({ status }: { status: boolean }) {
-  return (
-    <button type="submit" disabled={status}>
-      <span className={font_primary.className}>CADASTRAR</span>
-    </button>
-  )
-}
